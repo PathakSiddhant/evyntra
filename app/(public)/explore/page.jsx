926bb8dab840001a021fb6e192e349b559d991df
PathakSiddhant <siddhantpathak1207@gmail.com>
2025-12-21 12:39:37 +0530

    Created custom hooks for Query and Mutation

--- app/(public)/explore/page.jsx
+++ app/(public)/explore/page.jsx
@@ -1,9 +1,16 @@
+"use client"
+
+import { api } from '@/convex/_generated/api'
+import { useQuery } from 'convex/react'
+import { da } from 'date-fns/locale'
 import React from 'react'
 
 const ExplorePage = () => {
-  return (
-    <div>ExplorePage</div>
-  )
+  const data = useQuery(api.events.getFeaturedEvents);
+  
+  console.log(data);
+
+  return <div>ExplorePage</div>
 }
 
 export default ExplorePage
